7de287e45910e5d6f8a3b2cdf515524e0481e924
Gryphon5549 <firstteam5549@gmail.com>
2023-02-06 16:32:07 -0500

    Finished Intake Command

--- src/main/java/frc/robot/commands/RunIntake.java
+++ src/main/java/frc/robot/commands/RunIntake.java
@@ -19,16 +19,20 @@ public class RunIntake extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    
+    m_intake.intake_out();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_intake.run_intake();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    m_intake.retract_intake();
+  }
 
   // Returns true when the command should end.
   @Override
