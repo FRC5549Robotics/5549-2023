67d00d7efe8f9775cb453c2e2abc96c94c3f06b9
Gryphon Robotics <firstteam5549@gmail.com>
2023-03-09 15:01:59 -0500

    AutoAlign testing changes

--- src/main/java/frc/robot/commands/AutoAlignCommands/AutoAlign2X.java
+++ src/main/java/frc/robot/commands/AutoAlignCommands/AutoAlign2X.java
@@ -43,9 +43,11 @@ public class AutoAlign2X extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    finished = true;
     System.out.println(m_Limelight.getTx());
     controller2 = new PIDController(Constants.kP, Constants.kI, Constants.kD);
     if(m_Limelight.getTx() > 3 || m_Limelight.getTx()<-3){
+      System.out.println("trying to drive");
       m_drivetrain.drive(new ChassisSpeeds(controller2.calculate(m_Limelight.getTx(), 0), 0, 0));
     }
     else{
